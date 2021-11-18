83915e43c347c2d9d7e5567510a9048b4b2d711c
Zakaria Kerkeb <hikkary@MacBook-Pro-de-Zakaria.local>
2021-11-18 11:12:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import devices from './config/devices'
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  useEffect(()=>{
+    axios.get('http://hp-api.herokuapp.com/api/characters').then(res => {
+      setCharacters(res.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container> 
+      {characters.map(chara => <Image src={chara.image}></Image>)}
+      <Navigation>Barre de navigation</Navigation>
+    </Container>
   );
 }
+
+const Navigation = styled.p`
+
+  background-color:red;
+`
+
+const Image = styled.img`
+  margin-bottom: 12px;
+  border-radius: 50%;
+  width: 100%;
+  @media ${devices.tablet} {
+    /* width: 400px;
+    height: 400px; */
+  }
+`
+
+const Container = styled.div`
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 12px;
+  box-sizing: border-box;
+  align-items: center;
+  @media ${devices.tablet} {
+    background-color: green;
+  }
+  @media ${devices.laptop} {
+    flex-direction: column-reverse;
+    /* width: 400px;
+    height: 400px; */
+  }
+`
 
 export default App;
---
+++ src/config/devices.js
@@ -0,0 +1,13 @@
+const size = {
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+  }
+
+   const devices = {
+    mobile: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+  }; 
+
+  export default devices
